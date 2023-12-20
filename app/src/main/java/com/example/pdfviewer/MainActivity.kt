7a7ee31a18e718e2c00a46d87277424ad5f0efa3
Vlad <Vlad17.lutsk@gmail.com>
2023-12-20 19:55:32 +0200

    modify_class_in_MainActivity

--- app/src/main/java/com/example/pdfviewer/MainActivity.kt
+++ app/src/main/java/com/example/pdfviewer/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.pdfviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.pdfviewer.databinding.ActivityMainBinding
+import com.google.android.gms.instantapps.Launcher
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private val launcher =   registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ){uri ->
+        uri?.let {
+            binding.pdfView.fromUri(it).load()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.floatingActionButton.setOnClickListener{
+
+            launcher.launch("application/pdf")
+
+        }
     }
+
 }
