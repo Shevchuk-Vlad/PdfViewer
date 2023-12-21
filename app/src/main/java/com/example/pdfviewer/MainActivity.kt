6ccbb86383e8267d33c515517de8e52c92982851
Vlad <Vlad17.lutsk@gmail.com>
2023-12-21 22:01:10 +0200

    add the ability to open the file with app

--- app/src/main/java/com/example/pdfviewer/MainActivity.kt
+++ app/src/main/java/com/example/pdfviewer/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.pdfviewer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.pdfviewer.databinding.ActivityMainBinding
-import com.google.android.gms.instantapps.Launcher
 
 class MainActivity : AppCompatActivity() {
+
 
     private lateinit var binding : ActivityMainBinding
 
@@ -18,17 +19,30 @@ class MainActivity : AppCompatActivity() {
             binding.pdfView.fromUri(it).load()
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.floatingActionButton.setOnClickListener{
+        // Check if the activity was started with an intent
+        if (intent.action == Intent.ACTION_VIEW && intent.type == "application/pdf") {
+            val pdfUri = intent.data
+            pdfUri?.let {
+                openPdfFromUri(it)
+            }
+        }
 
+        binding.floatingActionButton.setOnClickListener {
             launcher.launch("application/pdf")
-
+            // Add your logic to handle opening PDF files (e.g., launching a file picker)
         }
     }
 
+    private fun openPdfFromUri(uri: Uri) {
+        // Display the PDF using the provided URI
+        binding.pdfView.fromUri(uri).load()
+    }
 }
+
